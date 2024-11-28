6c1f6159cd7e7e18dbec22b18e89ce927b9744cf
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2024-11-28 07:42:50 -0700

    Update masse-walker.c

--- cfiles/masse-walker.c
+++ cfiles/masse-walker.c
@@ -111,8 +111,8 @@ double
 factorial(int n)
 {
 	int i;
-	double f = 1.0;
-	for (i = 1; i <= n; i++)
-		f *= (double) i;
-	return f;
+	double d = 1.0;
+	for (i = 2; i <= n; i++)
+		d *= (double) i;
+	return d;
 }
